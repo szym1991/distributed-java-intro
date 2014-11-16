4fbdac2b190dc9c87f839e06e2afe53c29c39dd4
Szymon Apolinarski <szymon.apolinarski@gmail.com>
2014-11-16 12:45:45 +0100

    random people names

--- projects/1-threads/students/CharityFleaMarket/src/main/java/pl/edu/amu/wmi/charityfleamarket/Main.java
+++ projects/1-threads/students/CharityFleaMarket/src/main/java/pl/edu/amu/wmi/charityfleamarket/Main.java
@@ -3,6 +3,7 @@ package pl.edu.amu.wmi.charityfleamarket;
 import java.util.concurrent.TimeUnit;
 import pl.edu.amu.wmi.charityfleamarket.roles.Donor;
 import pl.edu.amu.wmi.charityfleamarket.roles.MarketManager;
+import pl.edu.amu.wmi.charityfleamarket.roles.People;
 import pl.edu.amu.wmi.charityfleamarket.roles.Recipient;
 
 /**
@@ -10,25 +11,25 @@ import pl.edu.amu.wmi.charityfleamarket.roles.Recipient;
  * @author Szymon <szymon.apolinarski@gmail.com>
  */
 public class Main {
+
     public static void main(String[] args) {
-        
+
         MarketManager manager = MarketManager.getInstance();
 
-        for(int i=1; i<=5; i++){
-            manager.addParticipant(new Donor("Donor " + i));
+        for (int i = 0; i < 5; i++) {
+            manager.addParticipant(new Donor("Donor " + People.getRandomName()));
         }
 
-        for(int i=1; i<=10; i++){
-            manager.addParticipant(new Recipient("Recipient " + i));
+        for (int i = 0; i < 10; i++) {
+            manager.addParticipant(new Recipient("Recipient " + People.getRandomName()));
         }
 
         manager.start();
 
-        //TO AVOID ENDLESS AUCTION
         try {
             TimeUnit.MINUTES.sleep(1);
         } catch (InterruptedException e) {
-        
+
         }
 
         manager.close();
