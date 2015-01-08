ea252803f0acb240ee4743c0f63936014abc732e
Szymon Apolinarski <szymon.apolinarski@gmail.com>
2015-01-08 08:20:11 +0100

    labs10

---
+++ 10-rest-web-service/students/restful/books/src/main/java/pl/edu/amu/dji/jms/lab10/books/service/GreetingService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.dji.jms.lab10.books.service;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.edu.amu.dji.jms.lab10.books.model.Greeting;
+
+/**
+ *
+ * @author Szymon
+ */
+@RestController
+public class GreetingService {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    @ResponseBody
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
